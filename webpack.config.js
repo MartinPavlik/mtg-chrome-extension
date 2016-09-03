f11451a0312e7f5e263b2cb1e72a4dc395732035
MartinPavlik <martin.pavlik100@gmail.com>
2016-09-04 01:36:46 +0200

    Fixed indentation of package.json & installed stylus-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        filename: './extension/app/build/app.js' //this is the default name, so you can skip it
-    },
-    module: {
+  entry: './src/index.jsx',
+  output: {
+    filename: './extension/app/build/app.js' //this is the default name, so you can skip it
+  },
+  module: {
     loaders: [
-        {
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel', // 'babel-loader' is also a legal name to reference
-            query: {
-                presets: ['react', 'es2015']
-            }
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+            presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.styl$/,
+        loader: 'css-loader!stylus-loader?paths=node_modules/bootstrap-stylus/stylus/'
+      }
     ]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
 }
